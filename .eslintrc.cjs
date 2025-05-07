828651bacd2c2125ede3ac1ac6f810dae814bfec
sl1dee <118618836+sl1dee@users.noreply.github.com>
2025-05-07 15:50:26 +0300

    refactor: refactor config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,24 +35,42 @@ module.exports = {
         'import/resolver': {
             node: {
                 extensions: ['.js', '.jsx', '.ts', '.tsx']
+            },
+            typescript: {},
+            alias: {
+                map: [
+                    ['@app', './src/app'],
+                    ['@processes', './src/processes'],
+                    ['@pages', './src/pages'],
+                    ['@widgets', './src/widgets'],
+                    ['@features', './src/features'],
+                    ['@entities', './src/entities'],
+                    ['@shared', './src/shared']
+                ],
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
             }
         }
     },
     rules: {
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'no-console': ['warn', {allow: ['warn', 'error']}],
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['warn'],
         'prefer-const': 'warn',
-
         'react/prop-types': 'off',
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
-
         'import/order': [
             'warn',
             {
                 groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-                'newlines-between': 'always',
+                pathGroups: [
+                    {
+                        pattern: '{@app,@processes,@pages,@widgets,@features,@entities,@shared}/**',
+                        group: 'internal',
+                        position: 'before'
+                    }
+                ],
+                'newlines-between': 'never',
                 alphabetize: {
                     order: 'asc',
                     caseInsensitive: true
