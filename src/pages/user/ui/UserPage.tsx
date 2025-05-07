c3ec778341edff66c210c7074f6ce5354200183e
sl1dee <118618836+sl1dee@users.noreply.github.com>
2025-05-07 16:10:58 +0300

    feat: make UserPage

---
+++ src/pages/user/ui/UserPage.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@features/auth/lib/useAuth.ts';
+import cl from './UserPage.module.scss';
+
+const UserPage = () => {
+  const { user, signOut } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut();
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <div className={cl.wrapper}>
+      <h2>User information</h2>
+      <div className={cl.info}>
+        <div>Lastname: {user?.lastname}</div>
+        <div>Firstname: {user?.firstname}</div>
+        <div>Email: {user?.email}</div>
+        <div>Phone: {user?.phone}</div>
+        <div>Website: {user?.website}</div>
+        <div>Gender: {user?.gender}</div>
+      </div>
+      <button onClick={handleSignOut} className={cl.btn}>
+        Sign out
+      </button>
+    </div>
+  );
+};
+
+export default UserPage;
